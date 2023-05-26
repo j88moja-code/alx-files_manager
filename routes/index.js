f163ec52362c26234c4be5b7d859a0e307e39621
j88moja-code <sitholent+github@gmail.com>
2023-05-26 14:16:50 +0200

    3. Create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-// import AuthController from '../controllers/AuthController';
-// import FilesController from '../controllers/FilesController';
 
 const express = require('express');
 
